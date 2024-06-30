7d8b0a485b16dad68d370ef7633a35ee747e3827
AkshatAgrawal247 <142831136+AkshatAgrawal247@users.noreply.github.com>
2024-06-30 08:56:35 +0000

    datatypes

---
+++ 01basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict" ;   // treat all JS code as newer version.
+
+// alert(3 + 3)    // we are using node js , not browser
+
+
+// <-------------datatypes------------>
+
+let name = "Akshat"   // string
+let age = 20    // number 
+let isLoggedIn = false      //boolean
+let value = null        // output of typeof null us object and output of undefined is undefined
+let state ;  // undefined
+
+
+console.table([typeof(name), typeof(age), typeof(isLoggedIn), typeof(value) , typeof(state)])
+
+/*
+number  => 2 to power 53 
+bigInt
+string => " "
+boolean => true/false
+null => standalone value
+undefined 
+symbol => unique
+
+//object 
+*/
