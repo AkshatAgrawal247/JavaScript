868bac84e64348f4329f230a22cb7ef1a3de7012
AkshatAgrawal247 <142831136+AkshatAgrawal247@users.noreply.github.com>
2024-06-30 10:01:36 +0000

    comparision

---
+++ 01basics/04_comparisions.js
@@ -0,0 +1,21 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 0);           // Equality "==" check  and comparisions ">= ,<= ,> ,< " are totally different thing in JS. and comparsions convert null into "0".
+// console.log(null == 0);
+// console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// // ===      // it checks values along with the datatypes.
+
+// console.log("2" === 2);
